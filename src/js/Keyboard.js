e176171e0295d0b401b40aa3371f37b759ced086
Ilya Ivchenkov <gunt911@gmail.com>
2022-05-09 02:36:10 +0300

    fix: class keyboard

--- src/js/Keyboard.js
+++ src/js/Keyboard.js
@@ -1,7 +1,8 @@
 export default class Keyboard {
 
     constructor(keys) {
-        this.keys = keys
+        this.keys = keys;
+        this.shiftOn = false;
     }
 
     renderKeys() {
@@ -25,18 +26,76 @@ export default class Keyboard {
             keyRow.classList.add('key-row');
             keyboardArea.append(keyRow);
             el.forEach(keyValaues => {
+
                 let keyDiv = document.createElement('div');
                 keyDiv.classList.add('key');
-                keyDiv.classList.add(`symbol${keyValaues.key}`);
-                keyDiv.textContent = keyValaues.key;
+                keyDiv.classList.add(keyValaues.code);
+
+                let spanRus = document.createElement('span');
+                spanRus.classList.add('span-rus');
+                keyDiv.append(spanRus);
+
+                let spanShiftOff = document.createElement('span');
+                spanShiftOff.classList.add('shift-off');
+                if(keyValaues.ru) {
+                    spanShiftOff.textContent = keyValaues.ru.key;
+                }
+                spanRus.append(spanShiftOff);
+
+                let spanShiftOn = document.createElement('span');
+                spanShiftOn.classList.add('shift-on', 'hide');
+                spanShiftOn.textContent = keyValaues.shiftKey;
+                spanRus.append(spanShiftOn);
+
+                let spanCapsOn = document.createElement('span');
+                spanCapsOn.classList.add('caps-on', 'hide');
+                spanCapsOn.textContent = keyValaues.shiftKey;
+                spanRus.append(spanCapsOn);
+
+                let spanCapsShift = document.createElement('span');
+                spanCapsShift.classList.add('caps-shift', 'hide');
+                spanCapsShift.textContent = keyValaues.shiftKey;
+                spanRus.append(spanCapsShift);   
+                
+                let spanEn = document.createElement('span');
+                spanRus.classList.add('span-en');
+                keyDiv.append(spanEn);
+
+                let spanShiftOffEn = document.createElement('span');
+                spanShiftOffEn.classList.add('shift-off');
+                spanShiftOffEn.textContent = keyValaues.key;
+                spanRus.append(spanShiftOffEn);
+
+                let spanShiftOnEn = document.createElement('span');
+                spanShiftOnEn.classList.add('shift-on', 'hide');
+                spanShiftOnEn.textContent = keyValaues.shiftKey;
+                spanRus.append(spanShiftOnEn);
+
+                let spanCapsOnEn = document.createElement('span');
+                spanCapsOnEn.classList.add('caps-on', 'hide');
+                spanCapsOnEn.textContent = keyValaues.shiftKey;
+                spanRus.append(spanCapsOnEn);
+
+                let spanCapsShiftEn = document.createElement('span');
+                spanCapsShiftEn.classList.add('caps-shift', 'hide');
+                spanCapsShiftEn.textContent = keyValaues.shiftKey;
+                spanRus.append(spanCapsShiftEn);   
+
                 keyRow.append(keyDiv);
+
                 keyDiv.addEventListener('click', () => {
                     this.buttonPressed(keyValaues);
                 })
                 document.addEventListener('keydown', (event) => {
                     event.preventDefault();
-                    if (event.key === keyValaues.key) {
+                    if (event.code === keyValaues.code) {
                         this.buttonPressed(keyValaues);
+                    }
+                })
+                document.addEventListener('keyup', (event) => {
+                    event.preventDefault();
+                    if (event.code === keyValaues.code) {
+                        this.buttonUnpressed(keyValaues);
                     }
                 })
             })
@@ -58,8 +117,14 @@ export default class Keyboard {
                 this.textarea.value = this.textarea.value.slice(0, start) + this.textarea.value.slice(end + 1);   
                 this.textarea.focus();            
             }
+        } else if (symbol.key === 'Shift') {
+            this.shiftOn = true;
+            console.log(this.shiftOn);
         } else {
             let symb = symbol.key;
+            if (this.shiftOn) {
+                symb = symbol.shiftKey;
+            }
             if (symbol.key === 'Tab') {
                 symb = '\t';
             }
@@ -67,5 +132,12 @@ export default class Keyboard {
             start = start + 1;
         }
         this.textarea.selectionStart = this.textarea.selectionEnd = start;
+    }
+
+    buttonUnpressed(symbol) {
+        if (symbol.key === 'Shift') {
+            this.shiftOn = false;
+            console.log(this.shiftOn);
+        }
     }
 }
