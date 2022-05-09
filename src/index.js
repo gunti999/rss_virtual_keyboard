bdc6f8b8e3dcbe757348756b3567647be760b26b
Ilya Ivchenkov <gunt911@gmail.com>
2022-05-09 23:02:28 +0300

    fix: eslint

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import './styles/style.css';
 import Keyboard from './js/Keyboard';
-import { keyArr } from './js/keyObj';
+import keyArr from './js/keyObj';
+
+/* global document */
 
 const body = document.querySelector('body');
-
 const container = document.createElement('div');
+
 container.classList.add('container');
 body.append(container);
 
-let keyboardClass = new Keyboard(keyArr);
+const keyboardClass = new Keyboard(keyArr);
 container.append(keyboardClass.renderKeys());
-
--- src/js/Keyboard.js
+++ src/js/Keyboard.js
@@ -1,263 +1,263 @@
+/* global localStorage, document */
+
+function createElment(element, className) {
+  const elem = document.createElement(element);
+  elem.classList.add(className);
+  return elem;
+}
+
 export default class Keyboard {
+  constructor(keys) {
+    this.keys = keys;
+    this.shiftOn = false;
+    this.capsOn = false;
+    this.ctrlOn = false;
+    this.altOn = false;
+    this.lang = localStorage.getItem('lang') || 'en';
+  }
 
-    constructor(keys) {
-        this.keys = keys;
-        this.shiftOn = false;
-        this.capsOn = false;
-        this.ctrlOn = false;
-        this.altOn = false;
-        this.lang = localStorage.getItem('lang') || 'en';
-    }
+  renderKeys() {
+    const virtualKeyboard = createElment('div', 'virtual-keyboard');
+    this.virtualKeyboard = virtualKeyboard;
 
-    createElment(element, className) {
-        let elem = document.createElement(element);
-        elem.classList.add(className);
-        return elem;
-    }
+    const h1 = createElment('h1', 'project-name');
+    h1.textContent = 'RSS Виртуальная клавиатура';
+    virtualKeyboard.append(h1);
 
-    renderKeys() {
+    const textarea = createElment('textarea', 'textarea');
+    this.textarea = textarea;
+    virtualKeyboard.append(textarea);
 
-        const virtualKeyboard = this.createElment('div', 'virtual-keyboard');
+    const keyboardArea = createElment('div', 'keyboard-area');
+    virtualKeyboard.append(keyboardArea);
 
-        const h1 = this.createElment('h1', 'project-name');
-        h1.textContent = "RSS Виртуальная клавиатура";
-        virtualKeyboard.append(h1);
+    this.keys.forEach((el) => {
+      const keyRow = createElment('div', 'key-row');
+      keyboardArea.append(keyRow);
 
-        const textarea = this.textarea = this.createElment('textarea', 'textarea');
-        virtualKeyboard.append(textarea);
+      el.forEach((keyValues) => {
+        const keyDiv = createElment('div', 'key');
+        keyDiv.classList.add(keyValues.code);
 
-        const keyboardArea = this.createElment('div', 'keyboard-area');
-        virtualKeyboard.append(keyboardArea);
+        const keyEn = keyValues;
+        const keyRu = keyValues.ru || keyValues;
 
-        this.keys.forEach(el => {
+        // RU
 
-            const keyRow = this.createElment('div', 'key-row');
-            keyboardArea.append(keyRow);
-
-            el.forEach(keyValues => {
-
-                const keyDiv = this.createElment('div', 'key');
-                keyDiv.classList.add(keyValues.code);
-
-                const keyEn = keyValues;
-                const keyRu = keyValues.ru || keyValues;
-
-                // RU
-
-                const spanRus = this.createElment('span', 'span-ru');
-                if (this.lang === 'ru') {
-                    spanRus.classList.remove('hide');
-                } else {
-                    spanRus.classList.add('hide');
-                }
-                
-                keyDiv.append(spanRus);
-
-                let spanShiftOff = this.createElment('span', 'shift-off');
-                spanShiftOff.textContent = keyRu.key;
-                spanRus.append(spanShiftOff);
-
-                let spanShiftOn = this.createElment('span', 'shift-on');
-                spanShiftOn.classList.add('hide');
-                spanShiftOn.textContent = keyRu.shiftKey;
-                spanRus.append(spanShiftOn);
-
-                let spanCapsOn = this.createElment('span', 'caps-on');
-                spanCapsOn.classList.add('hide');
-                if (keyRu.notCaps) {
-                    spanCapsOn.textContent = keyRu.key;
-                } else {
-                    spanCapsOn.textContent = keyRu.shiftKey;
-                }
-                spanRus.append(spanCapsOn);
-
-                let spanCapsShift = this.createElment('span', 'caps-shift');
-                spanCapsShift.classList.add('hide');
-                if (keyRu.notCaps) {
-                    spanCapsShift.textContent = keyRu.shiftKey;
-                } else {
-                    spanCapsShift.textContent = keyRu.key;
-                }
-                spanRus.append(spanCapsShift);
-
-                // EN
-
-                const spanEn = this.createElment('span', 'span-en');
-                if (this.lang === 'en') {
-                    spanEn.classList.remove('hide');
-                } else {
-                    spanEn.classList.add('hide');
-                }
-                keyDiv.append(spanEn);
-
-                let spanShiftOffEn = this.createElment('span', 'shift-off');
-                spanShiftOffEn.textContent = keyEn.key;
-                spanEn.append(spanShiftOffEn);
-
-                let spanShiftOnEn = this.createElment('span', 'shift-on');
-                spanShiftOnEn.classList.add('hide');
-                spanShiftOnEn.textContent = keyEn.shiftKey;
-                spanEn.append(spanShiftOnEn);
-
-                let spanCapsOnEn = this.createElment('span', 'caps-on');
-                spanCapsOnEn.classList.add('hide');
-                if (keyEn.notCaps) {
-                    spanCapsOnEn.textContent = keyEn.key;
-                } else {
-                    spanCapsOnEn.textContent = keyEn.shiftKey;
-                }
-                spanEn.append(spanCapsOnEn);
-
-                let spanCapsShiftEn = this.createElment('span', 'caps-shift');
-                spanCapsShiftEn.classList.add('hide');
-                if (keyEn.notCaps) {
-                    spanCapsShiftEn.textContent = keyEn.shiftKey;
-                } else {
-                    spanCapsShiftEn.textContent = keyEn.key;
-                }
-                spanEn.append(spanCapsShiftEn);
-
-                keyRow.append(keyDiv);
-
-                keyDiv.addEventListener('mousedown', () => {
-                    this.buttonPressed(keyValues);
-                })
-                keyDiv.addEventListener('mouseup', () => {
-                    this.buttonUnpressed(keyValues);
-                })
-                document.addEventListener('keydown', (event) => {
-                    event.preventDefault();
-                    if (event.code === keyValues.code) {
-                        this.buttonPressed(keyValues);
-                    }
-                })
-                document.addEventListener('keyup', (event) => {
-                    event.preventDefault();
-                    if (event.code === keyValues.code) {
-                        this.buttonUnpressed(keyValues);
-                    }
-                })
-            })
-        });
-        return virtualKeyboard;
-    }
-
-    buttonPressed(symbol) {
-        let start = this.textarea.selectionStart;
-        let end = this.textarea.selectionEnd;
-        const button = document.querySelector(`.${symbol.code}`);
-        button.classList.add('press');
-        if (symbol.key === 'Backspace') {
-            if (start > 0) {
-                this.textarea.value = this.textarea.value.slice(0, start - 1) + this.textarea.value.slice(end);
-                this.textarea.focus();
-                start = start - 1;
-            }
-        } else if (symbol.key === 'Delete') {
-            if (end < this.textarea.value.length) {
-                this.textarea.value = this.textarea.value.slice(0, start) + this.textarea.value.slice(end + 1);
-                this.textarea.focus();
-            }
-        } else if (symbol.key === 'Shift') {
-            this.shiftOn = true;
-            this.chekShiftFlag();
-        } else if (symbol.key === 'CapsLock') {
-            this.capsOn = !this.capsOn;
-            this.chekShiftFlag();
-        } else if (symbol.code === 'ControlLeft' || symbol.code === 'ControlRight') {
-            this.ctrlOn = true;
-            if (this.altOn) {
-                this.switchLang();
-            }
-        } else if (symbol.code === 'AltLeft' || symbol.code === 'AltRight') {
-            this.altOn = true;
-            if (this.ctrlOn) {
-                this.switchLang();
-            }
+        const spanRus = createElment('span', 'span-ru');
+        if (this.lang === 'ru') {
+          spanRus.classList.remove('hide');
         } else {
-            let symb = this.findKeyChar(symbol);
-            if (symbol.key === 'Tab') {
-                symb = '\t';
-            }
-            if (symbol.code === 'Space') {
-                symb = ' ';
-            }
-            if (symbol.code === 'Enter') {
-                symb = '\n';
-            }
-            this.textarea.value = this.textarea.value.slice(0, start) + symb + this.textarea.value.slice(end);
-            start = start + 1;
+          spanRus.classList.add('hide');
         }
-        this.textarea.selectionStart = this.textarea.selectionEnd = start;
-    }
 
-    findKeyChar(symbol) {
-        let char = document.querySelector(`.${symbol.code}`);
-        let span = char.querySelector('span:not(.hide) span:not(.hide)');
-        if (span) {
-            return span.textContent;
+        keyDiv.append(spanRus);
+
+        const spanShiftOff = createElment('span', 'shift-off');
+        spanShiftOff.textContent = keyRu.key;
+        spanRus.append(spanShiftOff);
+
+        const spanShiftOn = createElment('span', 'shift-on');
+        spanShiftOn.classList.add('hide');
+        spanShiftOn.textContent = keyRu.shiftKey;
+        spanRus.append(spanShiftOn);
+
+        const spanCapsOn = createElment('span', 'caps-on');
+        spanCapsOn.classList.add('hide');
+        if (keyRu.notCaps) {
+          spanCapsOn.textContent = keyRu.key;
         } else {
-            return '';
+          spanCapsOn.textContent = keyRu.shiftKey;
         }
-    }
+        spanRus.append(spanCapsOn);
 
-    buttonUnpressed(symbol) {
-        const button = document.querySelector(`.${symbol.code}`);
-        button.classList.remove('press');
-        if (symbol.code === 'ShiftLeft' || symbol.code === 'ShiftRight') {
-            this.shiftOn = false;
-            this.chekShiftFlag();
+        const spanCapsShift = createElment('span', 'caps-shift');
+        spanCapsShift.classList.add('hide');
+        if (keyRu.notCaps) {
+          spanCapsShift.textContent = keyRu.shiftKey;
+        } else {
+          spanCapsShift.textContent = keyRu.key;
         }
-        if (symbol.code === 'ControlLeft' || symbol.code === 'ControlRight') {
-            this.ctrlOn = false;
+        spanRus.append(spanCapsShift);
 
-        }
-        if (symbol.code === 'AltLeft' || symbol.code === 'AltRight') {
-            this.altOn = false;
-        }
-    }
+        // EN
 
-    switchLang() {
+        const spanEn = createElment('span', 'span-en');
         if (this.lang === 'en') {
-            this.lang = 'ru';
+          spanEn.classList.remove('hide');
         } else {
-            this.lang = 'en'
+          spanEn.classList.add('hide');
         }
-        localStorage.setItem('lang', this.lang);
-        this.checkLangFlag();
-    }
+        keyDiv.append(spanEn);
 
-    checkLangFlag() {
-        const keyLang = document.querySelectorAll('.key .span-en, .key .span-ru');
-        keyLang.forEach(el => {
-            if (el.classList.contains(`span-${this.lang}`)) {
-                el.classList.remove('hide');
-            } else {
-                el.classList.add('hide');
-            }
-        })
-    }
+        const spanShiftOffEn = createElment('span', 'shift-off');
+        spanShiftOffEn.textContent = keyEn.key;
+        spanEn.append(spanShiftOffEn);
 
-    chekShiftFlag() {
-        const key = document.querySelectorAll(`.key .span-${this.lang} span`);
-        let spanClass;
+        const spanShiftOnEn = createElment('span', 'shift-on');
+        spanShiftOnEn.classList.add('hide');
+        spanShiftOnEn.textContent = keyEn.shiftKey;
+        spanEn.append(spanShiftOnEn);
 
-        key.forEach(el => { el.classList.add('hide') });
-
-        if (this.shiftOn && this.capsOn) {
-            spanClass = 'caps-shift';
-        } else if (this.capsOn) {
-            spanClass = 'caps-on';
-        } else if (this.shiftOn) {
-            spanClass = 'shift-on';
+        const spanCapsOnEn = createElment('span', 'caps-on');
+        spanCapsOnEn.classList.add('hide');
+        if (keyEn.notCaps) {
+          spanCapsOnEn.textContent = keyEn.key;
         } else {
-            spanClass = 'shift-off';
+          spanCapsOnEn.textContent = keyEn.shiftKey;
         }
+        spanEn.append(spanCapsOnEn);
 
-        key.forEach(el => {
-            if (el.classList.contains(spanClass)) {
-                el.classList.remove('hide');
-            }
-        })
+        const spanCapsShiftEn = createElment('span', 'caps-shift');
+        spanCapsShiftEn.classList.add('hide');
+        if (keyEn.notCaps) {
+          spanCapsShiftEn.textContent = keyEn.shiftKey;
+        } else {
+          spanCapsShiftEn.textContent = keyEn.key;
+        }
+        spanEn.append(spanCapsShiftEn);
+
+        keyRow.append(keyDiv);
+
+        keyDiv.addEventListener('mousedown', () => {
+          this.buttonPressed(keyValues);
+        });
+        keyDiv.addEventListener('mouseup', () => {
+          this.buttonUnpressed(keyValues);
+        });
+        document.addEventListener('keydown', (event) => {
+          event.preventDefault();
+          if (event.code === keyValues.code) {
+            this.buttonPressed(keyValues);
+          }
+        });
+        document.addEventListener('keyup', (event) => {
+          event.preventDefault();
+          if (event.code === keyValues.code) {
+            this.buttonUnpressed(keyValues);
+          }
+        });
+      });
+    });
+    return virtualKeyboard;
+  }
+
+  buttonPressed(symbol) {
+    let start = this.textarea.selectionStart;
+    const end = this.textarea.selectionEnd;
+    const { value } = this.textarea;
+    const button = this.virtualKeyboard.querySelector(`.${symbol.code}`);
+    button.classList.add('press');
+    if (symbol.key === 'Backspace') {
+      if (start > 0) {
+        this.textarea.value = value.slice(0, start - 1) + value.slice(end);
+        this.textarea.focus();
+        start -= 1;
+      }
+    } else if (symbol.key === 'Delete') {
+      if (end < this.textarea.value.length) {
+        this.textarea.value = value.slice(0, start) + value.slice(end + 1);
+        this.textarea.focus();
+      }
+    } else if (symbol.key === 'Shift') {
+      this.shiftOn = true;
+      this.chekShiftFlag();
+    } else if (symbol.key === 'CapsLock') {
+      this.capsOn = !this.capsOn;
+      this.chekShiftFlag();
+    } else if (symbol.code === 'ControlLeft' || symbol.code === 'ControlRight') {
+      this.ctrlOn = true;
+      if (this.altOn) {
+        this.switchLang();
+      }
+    } else if (symbol.code === 'AltLeft' || symbol.code === 'AltRight') {
+      this.altOn = true;
+      if (this.ctrlOn) {
+        this.switchLang();
+      }
+    } else {
+      let symb = this.findKeyChar(symbol);
+      if (symbol.key === 'Tab') {
+        symb = '\t';
+      }
+      if (symbol.code === 'Space') {
+        symb = ' ';
+      }
+      if (symbol.code === 'Enter') {
+        symb = '\n';
+      }
+      this.textarea.value = value.slice(0, start) + symb + value.slice(end);
+      start += 1;
     }
+    this.textarea.selectionStart = start;
+    this.textarea.selectionEnd = start;
+  }
+
+  findKeyChar(symbol) {
+    const char = this.virtualKeyboard.querySelector(`.${symbol.code}`);
+    const span = char.querySelector('span:not(.hide) span:not(.hide)');
+    if (span) {
+      return span.textContent;
+    }
+    return '';
+  }
+
+  buttonUnpressed(symbol) {
+    const button = this.virtualKeyboard.querySelector(`.${symbol.code}`);
+    button.classList.remove('press');
+    if (symbol.code === 'ShiftLeft' || symbol.code === 'ShiftRight') {
+      this.shiftOn = false;
+      this.chekShiftFlag();
+    }
+    if (symbol.code === 'ControlLeft' || symbol.code === 'ControlRight') {
+      this.ctrlOn = false;
+    }
+    if (symbol.code === 'AltLeft' || symbol.code === 'AltRight') {
+      this.altOn = false;
+    }
+  }
+
+  switchLang() {
+    if (this.lang === 'en') {
+      this.lang = 'ru';
+    } else {
+      this.lang = 'en';
+    }
+    localStorage.setItem('lang', this.lang);
+    this.checkLangFlag();
+  }
+
+  checkLangFlag() {
+    const keyLang = this.virtualKeyboard.querySelectorAll('.key .span-en, .key .span-ru');
+    keyLang.forEach((el) => {
+      if (el.classList.contains(`span-${this.lang}`)) {
+        el.classList.remove('hide');
+      } else {
+        el.classList.add('hide');
+      }
+    });
+  }
+
+  chekShiftFlag() {
+    const key = this.virtualKeyboard.querySelectorAll(`.key .span-${this.lang} span`);
+    let spanClass;
+
+    key.forEach((el) => { el.classList.add('hide'); });
+
+    if (this.shiftOn && this.capsOn) {
+      spanClass = 'caps-shift';
+    } else if (this.capsOn) {
+      spanClass = 'caps-on';
+    } else if (this.shiftOn) {
+      spanClass = 'shift-on';
+    } else {
+      spanClass = 'shift-off';
+    }
+
+    key.forEach((el) => {
+      if (el.classList.contains(spanClass)) {
+        el.classList.remove('hide');
+      }
+    });
+  }
 }
